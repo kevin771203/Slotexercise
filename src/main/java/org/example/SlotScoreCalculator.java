3d166007e85c2c2cf7203cdb9e9b102ef003a684
Kevin <kevin@amigosoftware.com.tw>
2025-01-02 16:25:54 +0800

    one line

--- src/main/java/org/example/SlotScoreCalculator.java
+++ src/main/java/org/example/SlotScoreCalculator.java
@@ -1,8 +1,31 @@
 package org.example;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class SlotScoreCalculator {
 
-    public int calculate() {
-        return 0;
+    private final List<List<String>> wheels;
+
+    public SlotScoreCalculator(List<List<String>> wheels) {
+        this.wheels = wheels;
+    }
+
+    public int calculate(int bet) {
+
+        int odd = 0;
+        for (int i =0; i<3; i++) {
+
+            int finalI = i;
+            Set<String> distinctSymbols = wheels.stream().map(wheel -> wheel.get(finalI)).collect(Collectors.toSet());
+
+            if (distinctSymbols.size() == 1) {
+                odd = 40;
+                break;
+            }
+        }
+
+        return odd * bet;
     }
 }
