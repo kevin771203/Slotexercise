32d2433cd1c2e919cf29e789012df27873609077
Kevin <kevin@amigosoftware.com.tw>
2025-01-02 16:25:54 +0800

    one line

--- src/test/java/org/example/SlotScoreCalculatorTest.java
+++ src/test/java/org/example/SlotScoreCalculatorTest.java
@@ -3,12 +3,40 @@ package org.example;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class SlotScoreCalculatorTest {
-    @Test
-    void name() {
-        SlotScoreCalculator sut = new SlotScoreCalculator();
+import java.util.List;
 
-        int win = sut.calculate();
+class SlotScoreCalculatorTest {
+
+    @Test
+    void one_line() {
+        SlotScoreCalculator sut = new SlotScoreCalculator(
+                List.of(
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "2", "3")
+                )
+        );
+
+        int win = sut.calculate(10);
+
+        Assertions.assertThat(win).isEqualTo(400);
+    }
+
+    @Test
+    void lose() {
+        SlotScoreCalculator sut = new SlotScoreCalculator(
+                List.of(
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("A", "1", "2"),
+                        List.of("1", "2", "3")
+                )
+        );
+
+        int win = sut.calculate(10);
 
         Assertions.assertThat(win).isEqualTo(0);
     }
