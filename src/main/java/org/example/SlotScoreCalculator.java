9577fc69432730d0266238e49792367430589795
Kevin <b0952193319@gmail.com>
2025-01-06 10:50:31 +0800

    extract field

--- src/main/java/org/example/SlotScoreCalculator.java
+++ src/main/java/org/example/SlotScoreCalculator.java
@@ -7,20 +7,27 @@ public class SlotScoreCalculator {
 
     private final List<List<String>> wheels;
 
+    private final Map<Integer, Integer> odds = Map.ofEntries(
+            new AbstractMap.SimpleEntry<>(0, 0),
+            new AbstractMap.SimpleEntry<>(1, 10),
+            new AbstractMap.SimpleEntry<>(2, 40),
+            new AbstractMap.SimpleEntry<>(3, 100)
+    );
+
     public SlotScoreCalculator(List<List<String>> wheels) {
         this.wheels = wheels;
     }
 
     public int calculate(int bet) {
 
-        Random random = new Random();
-
-        for (List<String> wheel : wheels) {
-            int index = random.nextInt(wheels.size());
-            wheel.subList(index, wheels.size() + index);
-        }
-        
-        // ---------------
+//        Random random = new Random();
+//
+//        for (List<String> wheel : wheels) {
+//            int index = random.nextInt(wheels.size());
+//            wheel.subList(index, wheels.size() + index);
+//        }
+//
+//        // ---------------
 
         int odd = getOdd();
 
@@ -35,12 +42,7 @@ public class SlotScoreCalculator {
 
     private int getOdd(int lines) {
 
-        Map<Integer, Integer> odds = Map.ofEntries(
-                new AbstractMap.SimpleEntry<>(0,0),
-                new AbstractMap.SimpleEntry<>(1,10),
-                new AbstractMap.SimpleEntry<>(2,40),
-                new AbstractMap.SimpleEntry<>(3,100)
-        );
+
         Integer odd = odds.get(lines);
 
         if (Objects.isNull(odd)) {
