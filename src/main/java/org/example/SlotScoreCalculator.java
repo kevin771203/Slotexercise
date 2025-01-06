a8bfe342a2ec1245148305df5ff5eceb3625e031
Kevin <b0952193319@gmail.com>
2025-01-06 15:00:37 +0800

    change Reels name

--- src/main/java/org/example/SlotScoreCalculator.java
+++ src/main/java/org/example/SlotScoreCalculator.java
@@ -5,9 +5,9 @@ public class SlotScoreCalculator {
     private final org.example.payTable payTable;
     private final Reels reels;
 
-    public SlotScoreCalculator(payTable payTable, Reels reels1) {
+    public SlotScoreCalculator(payTable payTable, Reels reels) {
         this.payTable = payTable;
-        this.reels = reels1;
+        this.reels = reels;
     }
 
     public int calculate(int bet) {
