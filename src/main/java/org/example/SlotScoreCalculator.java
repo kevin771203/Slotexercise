81688c2d25b633bf26f8dfb551948445f19a65b8
Kevin <b0952193319@gmail.com>
2025-01-06 14:47:58 +0800

    replace raw data with object : Reels

--- src/main/java/org/example/SlotScoreCalculator.java
+++ src/main/java/org/example/SlotScoreCalculator.java
@@ -4,20 +4,21 @@ import java.util.*;
 
 public class SlotScoreCalculator {
 
-    private final List<List<String>> rawReels;
+//    private final List<List<String>> rawReels;
 
     private final Random random;
     private final org.example.payTable payTable;
+    private final Reels reels1;
 
     public SlotScoreCalculator(List<List<String>> rawReels, Random random, payTable payTable) {
-        this.rawReels = rawReels;
+//        this.rawReels = rawReels;
         this.random = random;
         this.payTable = payTable;
+        reels1 = new Reels(rawReels);
     }
 
     public int calculate(int bet) {
 
-        Reels reels1 = new Reels(rawReels);
         Screen screen = reels1.reelsToScreen(this.random);
 
         int odd = payTable.getOdd(screen);
