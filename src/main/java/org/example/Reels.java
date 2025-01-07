d59ad79cc914b4b15263fe287038fad61b3e67f3
Kevin <b0952193319@gmail.com>
2025-01-07 23:18:57 +0800

    remove commented code

--- src/main/java/org/example/Reels.java
+++ src/main/java/org/example/Reels.java
@@ -2,36 +2,22 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 
 public final class Reels {
 
-//    private final List<List<String>> rawReels;
     private final List<Reel> reelList = new ArrayList<>();
     private final RandomNumberGenerator randomNumberGenerator;
-//    private Screen screen;
+
 
     public Reels(List<List<String>> rawReels, RandomNumberGenerator randomNumberGenerator) {
-//        this.rawReels = rawReels;
+
         this.randomNumberGenerator = randomNumberGenerator;
 
         for (List<String> rawReel : rawReels) {
             Reel reel = new Reel(rawReel);
             reelList.add(reel);
         }
-
-//        List<List<String>> rawScreen = rawReels().stream().map(
-//                reel -> {
-//                    int nextPosition = 0;
-//
-//                    return Stream.concat(reel.stream(), reel.stream()).toList().subList(
-//                            nextPosition, nextPosition + 3
-//                    );
-//                }
-//        ).toList();
-
-//        this.screen = new Screen(rawScreen);
 
     }
 
@@ -40,23 +26,8 @@ public final class Reels {
         for (Reel reel : reelList) {
             reel.roll(randomNumberGenerator);
         }
-//        List<List<String>> rawScreen = rawReels().stream().map(
-//                reel -> {
-//                    int nextPosition = randomNumberGenerator.nextInt(reel.size()); //command
-//
-//                    return Stream.concat(reel.stream(), reel.stream()).toList().subList( // mid
-//                            nextPosition, nextPosition + 3
-//                    );
-//                }
-//        ).toList();
 
-//        this.screen = new Screen(rawScreen); // query
     }
-
-//    List<List<String>> rawReels() {
-//        return rawReels;
-//    }
-
 
     public Screen getScreen() {
 
