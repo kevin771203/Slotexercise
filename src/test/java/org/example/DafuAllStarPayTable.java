e3af0c78302d0b6a4c10b7c1e458c4a9a2c9569d
Kevin <b0952193319@gmail.com>
2025-01-09 12:33:21 +0800

    200 and 400

--- src/test/java/org/example/DafuAllStarPayTable.java
+++ src/test/java/org/example/DafuAllStarPayTable.java
@@ -1,9 +1,31 @@
 package org.example;
 
+import java.util.List;
+
 public class DafuAllStarPayTable implements PayTable {
 
     @Override
     public int getOdd(Screen screen) {
-        return 400;
+
+        List<List<String>> rawScreen = screen.rawScreen();
+
+        int count = 0;
+        for (List<String> row : rawScreen) {
+
+            if (row.get(0).equals("A")) {
+
+                count++;
+            }else {
+
+                break;
+            }
+            
+        }
+
+        if (count == 5) {
+            return 400;
+        }
+
+        return 200;
     }
 }
--- src/test/java/org/example/DafuAllStarPayTableTest.java
+++ src/test/java/org/example/DafuAllStarPayTableTest.java
@@ -9,6 +9,25 @@ import java.util.List;
 class DafuAllStarPayTableTest {
 
     @Test
+    void A_4_200(){
+
+        PayTable sut =  new DafuAllStarPayTable();
+
+        Screen screen = new Screen(
+                List.of(
+                        List.of("A", "2", "3"),
+                        List.of("A", "4", "3"),
+                        List.of("A", "2", "3"),
+                        List.of("A", "4", "3"),
+                        List.of("2", "8", "4")
+                )
+        );
+        int actual = sut.getOdd(screen);
+
+        Assertions.assertThat(actual).isEqualTo(200);
+    }
+
+    @Test
     void A_5_400(){
 
         PayTable sut =  new DafuAllStarPayTable();
